7e208e6842fa80ed99c79134bc6eae40da0c1bd0
Odiri Teddie <odiriteddie.adamu@gmail.com>
2023-11-27 10:24:19 -0500

    optimized version of the codebase using react memo

--- src/component/Product.tsx
+++ src/component/Product.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, memo } from "react";
 import { ProductType } from "../context/ProductsProvider";
 import { ReducerActionType, ReducerAction } from "../context/CartProvider";
 
@@ -40,4 +40,18 @@ function Product({
   return content;
 }
 
-export default Product;
+function areProductsEqual(
+  { product: prevProduct, inCart: prevInCart }: PropsType,
+  { product: nextProduct, inCart: nextInCart }: PropsType
+) {
+  return Object.keys(prevProduct).every((key) => {
+    return (
+      prevProduct[key as keyof ProductType] ===
+        nextProduct[key as keyof ProductType] && prevInCart === nextInCart
+    );
+  });
+}
+
+const MemoizedProduct = memo<typeof Product>(Product, areProductsEqual);
+
+export default MemoizedProduct;
